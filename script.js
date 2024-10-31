4c684dba9b78fd0d946273d5642575d9ce921e1b
mmariami154 <mariami.kobakhidze.1@gmail.com>
2024-10-31 17:46:06 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-
-let score = 81
-if (score >= 90) {
-    console.log("A");
-} else if (score >= 80) {
-    console.log("B");
-} else if (score >= 70) {
-    console.log("C");
-} else if (score >= 60) {
-    console.log("D");
-} else {
-    console.log("F");
+function randomNumber(arr) {
+    if(arr.length == 0) {
+        return {
+            min:null,
+            max:null
+        }
+    }
+    let max = arr[0]
+    let min = arr[0]
+    for(let i =0; i<arr.length; i++ ){
+        if(max < arr[i]) {
+            max = arr[i];
+        }
+        if (min > arr[i]){
+            min = arr[i];
+        }
+    }
+    return {
+        min:min,
+        max:max
+    }
 }
+console.log(randomNumber([1,10]));
